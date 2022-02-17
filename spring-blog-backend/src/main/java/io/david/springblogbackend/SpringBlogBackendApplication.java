b87d21dca432339432edc26989b6a8d3f8658514
D6uez <dpantech@gmail.com>
2022-02-17 13:15:48 -0500

    Frontend and Backend project structures

---
+++ spring-blog-backend/src/main/java/io/david/springblogbackend/SpringBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package io.david.springblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogBackendApplication.class, args);
+	}
+
+}
---
+++ spring-blog-backend/src/test/java/io/david/springblogbackend/SpringBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.david.springblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
